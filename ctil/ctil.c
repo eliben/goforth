6761579a00fd4769af18e0c015379132b79d6677
Eli Bendersky <eliben@gmail.com>
2025-07-29 06:06:54 -0700

    Start new skeleton for ctil

---
+++ ctil/ctil.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main() {
+    // Example of a simple C program that prints "Hello, World!"
+    printf("Hello, World!\n");
+    
+    // Return success status
+    return 0;
+}
