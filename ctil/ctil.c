df451e465bcc52bc96ebe5c5931f4d7503fe7cce
Eli Bendersky <eliben@gmail.com>
2025-07-30 06:20:52 -0700

    int64_t and add stack

--- ctil/ctil.c
+++ ctil/ctil.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "zmalloc.h"
 
@@ -7,13 +8,20 @@ typedef struct {
   char mem[64 * 1024];
 
   // Points to the link slot of the latest defined word.
-  int latest;
+  int64_t latest;
 
   // Points to the place where the next word can be defined.
-  int here;
+  int64_t here;
 
   // Current program counter.
-  int pc;
+  int64_t pc;
+
+  // Forth data stack.
+  int64_t datastack[64 * 1024];
+  
+  // Pointer to the top item on the data stack.
+  int64_t stacktop;
+
 } state_t;
 
 state_t* create_state() {
@@ -21,11 +29,15 @@ state_t* create_state() {
   s->pc = 0;
   s->latest = 0;
   s->here = 0;
+  s->stacktop = -1;
   return s;
 }
 
 int main() {
   printf("Hello, World!\n");
+  state_t* state = create_state();
+
+
 
   return 0;
 }
