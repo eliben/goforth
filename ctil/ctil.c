f137b2491444acd7b86f8e15fc5fee1d98743ae6
Eli Bendersky <eliben@gmail.com>
2025-07-30 06:13:25 -0700

    Add fields

--- ctil/ctil.c
+++ ctil/ctil.c
@@ -5,12 +5,22 @@
 
 typedef struct {
   char mem[64 * 1024];
+
+  // Points to the link slot of the latest defined word.
+  int latest;
+
+  // Points to the place where the next word can be defined.
+  int here;
+
+  // Current program counter.
   int pc;
 } state_t;
 
 state_t* create_state() {
   state_t* s = (state_t*)zmalloc(sizeof(state_t));
   s->pc = 0;
+  s->latest = 0;
+  s->here = 0;
   return s;
 }
 
