428156e27466ab159531246f85ab56d165b21f3c
Eli Bendersky <eliben@gmail.com>
2025-08-07 17:44:25 -0700

    Add comment

--- ctil/ctil.c
+++ ctil/ctil.c
@@ -11,6 +11,11 @@ int main() {
   state_t* state = create_state();
   register_builtins(state);
   //   show_state(state, 0, state->here);
+
+  // TODO: the easiest way to load another file would be at runtime,
+  // add a flag e.g. -i <filename> to the command line. 
+  // Change `interpret` to accept a custom stream; call it once with the
+  // opened file, and then with stdin.
   interpret(state);
   return 0;
 }
